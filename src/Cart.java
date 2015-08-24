b08beb60f42ccb6db8bbc797c7035e9cf1f34688
priyadarshini <priyadr@thoughtworks.com>
2015-08-24 18:25:03 +0530

    receipt

--- src/Cart.java
+++ src/Cart.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 /**
  * Created by admin on 24/08/15.
  */
 public class Cart {
+    public Cart() {
+        ArrayList<Good> list = new ArrayList<Good>();
+    }
+    public Good addItem(int qty,String name,float cost){
+
+        Good product=null;
+        if(Shelf.instance().has(name,qty)){
+            product=shelf.instance.get(name,qty);
+        }
+        return product;
+
+
+    }
+
 }
--- src/Shelf.java
+++ src/Shelf.java
@@ -2,4 +2,14 @@
  * Created by admin on 24/08/15.
  */
 public class Shelf {
+
+    private Shelf(){}
+    private static Shelf ins;
+
+    public static Shelf instance(){
+        if(ins==null)
+            ins = new Shelf();
+        return ins;
+    }
+
 }
